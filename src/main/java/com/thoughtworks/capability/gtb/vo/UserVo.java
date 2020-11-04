9d7d4c9cd0c727b47b3a0f93fcd9016a2064ce5b
tanghaoran <407458281@qq.com>
2020-11-04 23:32:15 +0800

    add annotate for problem1

--- src/main/java/com/thoughtworks/capability/gtb/vo/UserVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/UserVo.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserVo {
-
+  @JsonProperty(value = "userId")
   private String id;
+  @JsonProperty(value = "userName")
   private String name;
 }
