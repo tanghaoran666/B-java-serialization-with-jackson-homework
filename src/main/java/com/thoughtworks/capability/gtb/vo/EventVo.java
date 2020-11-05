b2262e3552fe3955cedb4cbf42990fb8e7759578
tanghaoran666 <18408230362@163.com>
2020-11-05 23:21:56 +0800

    [thr] feat: implement homework2

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,9 +1,9 @@
 package com.thoughtworks.capability.gtb.vo;
 
 import java.util.Date;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.thoughtworks.capability.gtb.serializer.TimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +16,7 @@ public class EventVo {
   private String id;
   private String name;
   private EventType type;
-  @JsonFormat(pattern = "MS")
+  @JsonSerialize(using = TimeSerializer.class)
   private Date time;
   @JsonUnwrapped
   private UserVo user;
